eb5455bcb60f95762f949e3f2a92119b11dc128f
Christine Myers Keitt <cmyerskeitt@gmail.com>
2020-08-06 16:02:08 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,7 +4,8 @@ require_relative '../lib/concerns/paramable'
 require 'pry'
 
 class Artist
-  
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
   extend Findable 
   include Paramable
   
